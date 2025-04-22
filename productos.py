d59ade5c3eba2a777bbfe8b18a7e03ac406cc925
Johan192004 <121966452+Johan192004@users.noreply.github.com>
2025-04-22 17:08:38 -0500

    Add files via upload

---
+++ productos.py
@@ -0,0 +1,90 @@
+#Se pide el nombre al usuario
+nombre = input("Ingrese el nombre del producto: ")
+
+
+#Se pide el precio unitario al usuario
+precio_unitario = input("Ingrese el precio unitario: ")
+
+#Verifiacion de tipo de dato de precio unitario
+while True:
+    try:
+        precio_unitario = float(precio_unitario)
+
+    except ValueError as e:
+        print("Error, ingresaste un numero no valido")
+        precio_unitario = input("Ingrese de nuevo el precio unitario: ")
+
+    else:
+        
+        if precio_unitario >= 0:
+            break
+
+        else:
+            print("Error, ingresaste un numero no valido")
+            precio_unitario = input("Ingrese de nuevo el precio unitario: ")
+
+
+#Se pide el numero de productos adquiridos al usuario
+num_product_adquiridos = input("Ingrese la cantidad de productos adquiridos: ")
+
+#Verificacion de tipo de dato de numero de productos adquiridos
+while True:
+    try:
+        num_product_adquiridos = int(num_product_adquiridos)
+
+    except ValueError as e:
+        print("Error, ingresaste un numero no valido")
+        num_product_adquiridos = input("Ingrese de nuevo la cantidad de productos adquiridos: ")
+
+    else:
+
+        #Aqui se verifica que el tipo de dato no sea float y que sea mayor que 0
+        if isinstance(num_product_adquiridos,float) or num_product_adquiridos < 0:
+            print("Error, ingresaste un numero no valido")
+            num_product_adquiridos = input("Ingrese de nuevo la cantidad de productos adquiridos: ")
+
+        else:
+            break
+
+
+#Se pide el porcentaje de decuento al usuario
+porcen_descuento = input("Ingrese el porcentaje de descuento en un rango de 0 a 100: ")
+
+#Verificacion de tipo de dato de porcentaje de descuento
+while True:
+    try:
+        porcen_descuento = int(porcen_descuento)
+
+    except ValueError as e:
+        print("Error, ingresaste un numero no valido")
+        porcen_descuento = input("Ingrese de nuevo el porcentaje de descuento en un rango de 0 a 100: ")
+
+    else:
+
+        """Aqui se verifica que el tipo de dato no sea float 
+        y que el porcentaje de descuento este en el rango de 0 a 100"""
+        if isinstance(porcen_descuento,float):
+            print("Error, ingresaste un numero no valido")
+            porcen_descuento = input("Ingrese de nuevo el porcentaje de descuento en un rango de 0 a 100: ")
+
+        elif porcen_descuento < 0 or porcen_descuento > 100:
+            print("El porcentaje de descuento tiene que estar entre 0 y 100")
+            porcen_descuento = input("Ingrese de nuevo el porcentaje de descuento en un rango de 0 a 100: ")
+
+        else:
+            break
+
+
+costo_sin_descuento = num_product_adquiridos * precio_unitario
+
+costo_con_descuento = 0
+
+costo_total = num_product_adquiridos * precio_unitario
+
+
+
+if porcen_descuento >= 1:
+    costo_total = costo_sin_descuento - (costo_sin_descuento*porcen_descuento)/100
+
+
+print(f"Nombre del producto: {nombre} \nMonto total: {costo_total:.2f}$")
